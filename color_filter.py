d84ff6ad83221ad80bdb3cd9c12de4282c8f3e2f
Gerard <73176828+GerardNaharro@users.noreply.github.com>
2024-02-20 18:35:00 +0100

    Add files via upload

--- color_filter.py
+++ color_filter.py
@@ -1,12 +1,12 @@
 # Color filtering debugging
 
 import cv2
-
+import numpy as np
 
 
 # Open the image
 
-img = cv2.imread("imagenes/referencia1.PNG") # green
+img = cv2.imread("imagenes/portero_rojo.PNG") # green
 #img = cv2.imread("imagenes/referencia2.PNG") # red
 
 # In-Range Filtering
@@ -17,10 +17,13 @@ hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 red_mask = cv2.inRange(hsv, (0,77,75), (22,255,255))
 
+gk_mask = cv2.inRange(hsv, (29,99,172) ,(36,193,254))
+
 green_mask = cv2.inRange(hsv, (38,56,155), (91,204,255))
 
 
 out = cv2.bitwise_and(img, img, mask=green_mask)
+print(np.sum(out != 0))
 #inv_out = cv2.bitwise_and(sample_img, sample_img, inv_mask)
 
 cv2.imshow("Image", img)
